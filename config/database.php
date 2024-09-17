7a19a03a87d22b4d511ed89bd3d916aa7c9ee8c3
kemallt <kemallt@gmail.com>
2024-09-17 12:14:12 +0300

    fix error with sqlite database in config

--- config/database.php
+++ config/database.php
@@ -38,7 +38,7 @@ return [
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DATABASE_URL'),
-            'database' => env('DB_DATABASE', database_path('php_project_lvl4.sqlite')),
+            'database' => database_path(env('DB_DATABASE', 'database.sqlite')),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
